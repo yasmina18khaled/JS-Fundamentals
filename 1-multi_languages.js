b7fa7aebddf7e80c35a91cc5da2778dfd75484d5
YASMINA K <Kyasmina377@gmail.com>
2025-07-08 05:55:54 +0300

    some edits in file

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,4 +1,3 @@
-const myVar1 = 'C is fun';
-const myVar2 = 'Python is cool';
-const myVar3 = 'JavaScript is amazing';
-console.log(myVar1,myVar2,myVar3);
+console.log("C is fun");
+console.log("Python is cool");
+console.log("JavaScript is amazing");
