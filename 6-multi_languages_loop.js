cb6e3767f2bbad4ebf442e3efec5932c30432280
YASMINA K <Kyasmina377@gmail.com>
2025-07-12 21:38:57 +0300

    Add script that loops on the array and prints the output again

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,7 +1,4 @@
 const array = ["C is fun", "Python is cool", "JavaScript is amazing"];
-for(let i = 0; i<array.length ; i++){
-        
+for (let i = 0; i<array.length ; i++){
     console.log(array[i]);
-    
-
 }
