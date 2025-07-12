525d305eee6c3eb070e1681f602886c0cbc7eb96
YASMINA K <Kyasmina377@gmail.com>
2025-07-13 01:08:14 +0300

    Make a square

---
+++ 8-square.js
@@ -0,0 +1,15 @@
+const size = process.argv[2];
+const Size = Number(size);
+if(isNaN(Size)){
+    console.log('Missing size');
+} 
+
+for (let i = 0; i < Size; i++){
+    let row = '';
+    for (let y = 0; y < Size; y++){
+       row += 'x';
+      
+    }
+    console.log(row);
+   
+}
